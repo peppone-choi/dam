935f3b63faeaf697431d9d4975fe1f05233c1ce9
peppone-choi <peppone.choi@gmail.com>
2023-07-25 17:09:12 +0900

    feat: 댓글 추천 / 비추천 및 취소 API 작성

--- src/main/java/com/peppone/dam/like/service/LikeService.java
+++ src/main/java/com/peppone/dam/like/service/LikeService.java
@@ -1,5 +1,7 @@
 package com.peppone.dam.like.service;
 
+import com.peppone.dam.like.dto.CommentCancelLikeDto;
+import com.peppone.dam.like.dto.CommentLikeDto;
 import com.peppone.dam.like.dto.PostCancelLikeDto;
 import com.peppone.dam.like.dto.PostLikeDto;
 import com.peppone.dam.response.CommonResponse;
@@ -10,4 +12,8 @@ public interface LikeService {
   CommonResponse postLike(UserEntity user, PostLikeDto likeDto);
 
   CommonResponse cancelPostLike(UserEntity user, PostCancelLikeDto postCancelLikeDto);
+
+  CommonResponse commentLike(UserEntity user, CommentLikeDto commentLikeDto);
+
+  CommonResponse cancelCommentLike(UserEntity user, CommentCancelLikeDto commentCancelLikeDto);
 }
